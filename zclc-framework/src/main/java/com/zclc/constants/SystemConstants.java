5b899e811267b7e971e3b49ebfec1a9f3975d17a
Zhou Min <132646373+justzsomso@users.noreply.github.com>
2024-04-15 21:55:45 +0800

    feat(常量处理): 将经常使用的常量在该类中进行定义方便后续使用

---
+++ zclc-framework/src/main/java/com/zclc/constants/SystemConstants.java
@@ -0,0 +1,44 @@
+package com.zclc.constants;
+
+public class SystemConstants
+{
+    /**
+     *  文章是草稿
+     */
+    public static final int ARTICLE_STATUS_DRAFT = 1;
+    /**
+     *  文章是正常分布状态
+     */
+    public static final int ARTICLE_STATUS_NORMAL = 0;
+    /**
+     * 类别正常发布状态
+     */
+    public static final String CATEGORY_STATUS_NORMAL = "0";
+    /**
+     * 类别禁用
+     */
+    public static final String CATEGORY_STATUS_DISABLE = "1";
+
+    /**
+     *  友情链接审核通过
+     */
+    public static final String Link_STATUS_NORMAL = "1";
+    /**
+     *  友情链接审核未通过
+     */
+    public static final String Link_STATUS_DRAFT = "0";
+
+    /**
+     *  评论的rootId是否是-1，判断是否是根评论
+     */
+    public static final int COMMENT_ROOTID_DRAFT = -1;
+
+    /**
+     *  评论类型：友链评论
+     */
+    public static final String ARTICLE_TYPE_DRAFT = "1";
+    /**
+     *  评论类型：文章评论
+     */
+    public static final String ARTICLE_TYPE_NORMAL = "0";
+}
